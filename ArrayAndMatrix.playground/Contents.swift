ee85c0132546f4eac997ec459ea126ce5c7874fc
Anya <melxxova@gmail.com>
2020-08-03 16:15:11 +0500

    matrix is ready

--- ArrayAndMatrix.playground/Contents.swift
+++ ArrayAndMatrix.playground/Contents.swift
@@ -12,3 +12,41 @@ func reverseArray(_ array: inout[Int]) {
        (array[i],array[count - i - 1])  = (array[count - i - 1],array[i])
     }
 }
+
+// 2
+let N = 4
+let M = 5
+
+var matrix = RandomMatrix(rows: N, columns: M)
+print("\nOriginal Matrix:")
+matrix.show()
+matrix.changeOddToEvenRows()
+print("Changed Matrix:")
+matrix.show()
+
+class RandomMatrix {
+    
+    private var matrix : Array<Array<Int>>
+    
+    init(rows: Int, columns: Int) {
+        matrix = Array(repeating: Array(repeating: Int(), count: columns), count: rows)
+        for i in 0..<rows {
+            for j in 0..<columns {
+                matrix[i][j] = Int.random(in: 10..<100)
+            }
+        }
+    }
+    
+    func show() {
+        for i in 0..<matrix.count {
+            print(matrix[i])
+        }
+    }
+    
+    func changeOddToEvenRows() {
+        let rows = matrix.count
+        for i in stride(from: 0, to: rows - 1, by: 2) {
+            (matrix[i],matrix[i + 1])  = (matrix[ i + 1],matrix[i])
+        }
+    }
+}
