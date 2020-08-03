96deb9ad3cc0158ef6af3f0c96c0289b452327e7
Anya <melxxova@gmail.com>
2020-08-03 16:01:31 +0500

    task with an Array

--- ArrayAndMatrix.playground/Contents.swift
+++ ArrayAndMatrix.playground/Contents.swift
@@ -1,1 +1,14 @@
 import Foundation
+
+// 1
+var a : Array = [1, 2, 3, 4]
+print("Original array: \(a)")
+reverseArray(&a)
+print("Reversed array: \(a)")
+
+func reverseArray(_ array: inout[Int]) {
+    let count = array.count
+    for i in 0..<count/2 {
+       (array[i],array[count - i - 1])  = (array[count - i - 1],array[i])
+    }
+}
